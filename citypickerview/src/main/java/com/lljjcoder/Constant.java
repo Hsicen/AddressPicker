65557bddb7d229e04e4bc054954f3e4c5120b5fd
Hsicen <codinghuang@163.com>
2019-06-19 14:03:37 +0800

    修改城市数据

--- citypickerview/src/main/java/com/lljjcoder/Constant.java
+++ citypickerview/src/main/java/com/lljjcoder/Constant.java
@@ -8,5 +8,5 @@ package com.lljjcoder;
 
 public class Constant {
 
-    public static final String CITY_DATA = "china_city_data.json";
+    public static final String CITY_DATA = "sys_city.json";
 }
